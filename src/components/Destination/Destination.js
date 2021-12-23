bdfbbbb5d67cba4e93c4e9d63104887a370188b6
ousmanito <ousmane.209@gmail.com>
2021-12-23 18:44:48 +0100

    Fix the Background issues when changing location
    Use of useLocation and useEfect hooks, un peu confus mais ça marche ! On passe maintenant à l'onglet Destination

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Destination.css'
 
 const Destination = () => {
     return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
 import "./Home.css";
 
 
-const Home = () => {
-  return (
-    <div className="HomePage">
-    </div>
-  );
+const changeBg = (string) => {
+  console.log('ok fct')
+  document.getElementById('homepage').className= '';
+  document.getElementById('homepage').classList.add(`${string}Page`);
+}
+
+const Home = ()=> {
+  let location = useLocation();
+  console.log(location)
+  useEffect(()=> {
+    console.log(location.pathname.slice(1,))
+    if(location.pathname === '/') {
+      changeBg('home');
+    } else {
+      changeBg(location.pathname.slice(1,))
+    }
+  }
+  ,[location.pathname])
+  return <div id="homepage" className="HomePage"></div>;
 };
 
 export default Home;
