48c6c62dcbc33d0587a3df3af5ac292ca9b89511
ousmanito <ousmane.209@gmail.com>
2022-03-05 10:45:17 +0100

    destination grid

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -4,35 +4,45 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Destination = () => {
   return (
-    <div className="destination">
+    <>
       <div className="destinationText">
         <ul>
           <li>Pick your destination</li>
         </ul>
       </div>
-      <div className="destinationImage"></div>
-      <div className="destinationNav">
-        <ul className="destinationLinks">
-          <NavLink to='/destination/moon'
-          className={({isActive})=> (isActive ? 'navLinks-active' : "")}>
-            <li>Moon</li>
-          </NavLink>
-          <NavLink to='/destination/mars'
-          className={({isActive})=> (isActive ? 'navLinks-active' : "")}>
-            <li>Mars</li>
-          </NavLink>
-          <NavLink to='/destination/europa'
-          className={({isActive})=> (isActive ? 'navLinks-active' : "")}>
-            <li>Europa</li>
-          </NavLink>
-          <NavLink to='/destination/titan'
-          className={({isActive})=> (isActive ? 'navLinks-active' : "")}>
-            <li>Titan</li>
-          </NavLink>
-        <Outlet/>
-        </ul>
+      <div className="destination">
+        <div className="destinationImage"></div>
+        <div className="destinationNav">
+          <ul className="destinationLinks">
+            <NavLink
+              to="/destination/moon"
+              className={({ isActive }) => (isActive ? "navLinks-active" : "")}
+            >
+              <li className="grid-1">Moon</li>
+            </NavLink>
+            <NavLink
+              to="/destination/mars"
+              className={({ isActive }) => (isActive ? "navLinks-active" : "")}
+            >
+              <li className="grid-2">Mars</li>
+            </NavLink>
+            <NavLink
+              to="/destination/europa"
+              className={({ isActive }) => (isActive ? "navLinks-active" : "")}
+            >
+              <li className="grid-3">Europa</li>
+            </NavLink>
+            <NavLink
+              to="/destination/titan"
+              className={({ isActive }) => (isActive ? "navLinks-active" : "")}
+            >
+              <li className="grid-4">Titan</li>
+            </NavLink>
+            <Outlet />
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
